853499c064d80ba1aecb9ab88edf26a6cfce3c2c
gauravsinghal87 <95953555+gauravsinghal87@users.noreply.github.com>
2023-11-24 13:10:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//time picker function using Jquery on timepicker class
+$(document).ready(function (e) {
+    $('.timepicker').timepicker({
+        interval: 30,
+    });
+});
+
+
+
+//multiselect dropdown jquery code 
+
+// $(document).ready(function() {
+//     $('#send_to').multiselect();
+// });
